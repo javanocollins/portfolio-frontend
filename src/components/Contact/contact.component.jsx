418e4ab31efef31ba4e5068f9e3cd0f21105e102
javanocollins <jdcollins242@gmail.com>
2022-02-18 13:19:06 -0500

    update: contact section

---
+++ src/components/Contact/contact.component.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Contact = () => {
+    return (
+        <div className="container mx-auto grid grid-cols-2">
+            <div>
+                <h2 className="text-5xl font-medium">Contact</h2>
+                <p>I'm much easier to reach on Instagram</p>
+
+                <div>
+                    <p>javanocollins@gmail.com</p>
+                </div>
+                <div>
+                    <p>+1242 803 7736</p>
+                </div>
+            </div>
+            <div>Hello</div>
+        </div>
+    );
+};
+
+export default Contact;
